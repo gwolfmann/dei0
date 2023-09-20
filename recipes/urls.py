9f533ef90587f0faab12109a9e0294223c40edc3
gustavowolfmann <gwolfmann@gmail.com>
2023-09-20 01:43:00 -0300

    some corrections

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create_recipe/', views.create_recipe_view, name='create-recipe'),
     path('ingredients/delete/<int:ingredient_id>/', views.delete_ingredient_view, name='delete-ingredient'),  # New URL for deleting an ingredient
     path('ingredients/update/<int:ingredient_id>/', views.update_ingredient_view, name='update-ingredient'),  # New URL for updating an ingredient
     path('ingredients/all/', views.get_all_ingredients_view, name='get-all-ingredients'),  # New URL for all ingredients
